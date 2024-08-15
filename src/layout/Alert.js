46e5cde50a828743025f3ae94b110ebe21e32935
Renan Roseno <rosenorenan@gmail.com>
2024-08-15 18:40:56 -0300

    feat:add sweetalert config

---
+++ src/layout/Alert.js
@@ -0,0 +1,17 @@
+import Swal from "sweetalert2";
+
+export function showAlertSuccess(title, text) {
+  Swal.fire({
+    icon: "success",
+    title: title,
+    text: text,
+  });
+}
+
+export function showAlertError(title, text) {
+  Swal.fire({
+    icon: "error",
+    title: title,
+    text: text,
+  });
+}
