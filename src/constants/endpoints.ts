abaff0cf7532293de45e38704826c52963051ecc
Francisco Mateus <franciscomateus0119@gmail.com>
2024-08-22 07:45:42 -0300

    [Update] Configured to list to /api
    - React will now listen to :port/api
    - it was just :8000 before
    - Endpoints changed from '${SERVER}/endpoint/' to '${SERVER}/endpoint'

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,5 +1,5 @@
 import { SERVER } from "./config";
 
-export const GET_ALL = `${SERVER}/verificando_amostras_rotuladas/`;
-export const GET_UNIQUE_SAMPLE = `${SERVER}/lendo_amostra_para_rotulagem/`;
-export const SAVE_LABELED_SAMPLES = `${SERVER}/salvar_amostras_rotuladas/`;
+export const GET_ALL = `${SERVER}/verificando_amostras_rotuladas`;
+export const GET_UNIQUE_SAMPLE = `${SERVER}/lendo_amostra_para_rotulagem`;
+export const SAVE_LABELED_SAMPLES = `${SERVER}/salvar_amostras_rotuladas`;
