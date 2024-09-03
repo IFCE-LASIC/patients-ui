bd1847f82153d7150fdde2abece42d4b9a3429ed
Renan Roseno <rosenorenan@gmail.com>
2024-09-03 20:14:17 -0300

    fix: clean code on config file

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,9 +1,9 @@
 export const SERVER = "http://54.233.219.82:8000/api";
 
-export const mountHeader = (hash:string) => {
+export const mountHeader = (hash: string) => {
   return {
     headers: {
       "XCARDIO-API-KEY": hash,
     },
-  }
-}
+  };
+};
